cdef5fd837e8d814701f93fd1e5896054426accf
Hack Reactor Students <accounts@hackreactor.com>
2015-07-31 19:13:51 -0700

    validate url in html form

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,9 +3,15 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
   $scope.link = {};
+  $scope.valid = '';
 
   $scope.addLink = function(url) {
-     Links.postLink(url);
-     $scope.newUrl = '';
+    if (Links.isValidUrl(url)) {
+      Links.postLink(url);
+      $scope.newUrl = '';
+      $location.path('/links');
+    } else {
+      $scope.valid = 'you must be joking...';
+    }
   };
 });
